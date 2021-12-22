18ad59b618a0c3a3da3900d092b922492b511f41
4n4ru <ana.runje@live.com>
2021-12-22 13:25:11 +0000

    Feat: Set up allauth

--- book_hideout/settings.py
+++ book_hideout/settings.py
@@ -34,9 +34,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # If project is in development DEBUG = True, else DEBUG = False
 DEBUG = development
 
-# set ALLOWED_HOST depending if project is in development or not
+# settings depending if project is in development or not
 if development:
     ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -50,6 +51,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +84,24 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'book_hideout.wsgi.application'
 
