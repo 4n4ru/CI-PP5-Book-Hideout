fc1e85dba3275e6c1ac2e95626ef45f882535499
4n4ru <ana.runje@live.com>
2022-01-15 12:53:21 +0000

    Feat: Create Product and Genre models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,97 @@
+# Models for the products app
+
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    """
+    Class for the genre model
+    """
+    name = models.CharField(
+        max_length=254
+    )
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        """Returns the genres name as a string
+
+        Returns:
+            string: genre name
+        """
+        return f'{self.name}'
+
+    def get_friendly_name(self):
+        """Retruns the genres front end friendly name
+
+        Returns:
+            string: front end friendly name
+        """
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Class for the product model
+    """
+    genre = models.ManyToManyField(
+        'Genre',
+        null=True,
+        blank=True,
+    )
+    isbn = models.CharField(
+        max_length=254
+    )
+    isbn13 = models.CharField(
+        max_length=254
+    )
+    title = models.CharField(
+        max_length=254
+    )
+    subtitle = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    authors = models.CharField(
+        max_length=254
+    )
+    description = models.TextField()
+    publisher = models.CharField(
+        max_length=254
+    )
+    publication_date = models.DateField(
+        verbose_name='publication date'
+    )
+    format = models.CharField(
+        max_length=254
+    )
+    pages = models.IntegerField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+    rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1
+    )
+    image_url = models.URLField(
+        max_length=1024,
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        """Returns the products title as a string
+
+        Returns:
+            string: title
+        """
+        return f'{self.title}'
