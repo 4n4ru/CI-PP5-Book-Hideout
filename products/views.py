297a51ae38daaa79f8c08ac5ceda35bd8831e8bb
4n4ru <ana.runje@live.com>
2022-01-15 16:03:10 +0000

    Feat: Create a view for products, add url patterns

--- products/views.py
+++ products/views.py
@@ -1,3 +1,32 @@
+# Imports:
+# 3rd party:
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+# Internal:
+from .models import Product
+
+
+class AllProducts(View):
+    """A view to display the products page, including sorting and search queries
+
+    Args:
+        View (class): Built in parent class for views
+    """
+    def get(self, request):
+        """Renders the products page
+
+        Args:
+            request (object): HTTP request object
+
+        Returns:
+            method: renders products page
+        """
+
+        products = Product.objects.all()
+        
+        context = {
+            'products': products, 
+        }
+
+        return render(request, 'products/products.html', context)
