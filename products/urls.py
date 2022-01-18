53d431c62c8478c93cb67f4776a916c2850c8b25
4n4ru <ana.runje@live.com>
2022-01-18 13:17:45 +0000

    Feat: Add product details view, template and urls

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AllProducts.as_view(), name='products'),
+    path('<product_id>', views.ProductDetails.as_view(), name='product_details'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 # Imports:
 # 3rd party:
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 # Internal:
@@ -24,9 +24,34 @@ class AllProducts(View):
         """
 
         products = Product.objects.all()
-        
+
         context = {
             'products': products,
         }
 
         return render(request, 'products/products.html', context)
+
+
+class ProductDetails(View):
+    """A view to display the individual product details
+
+    Args:
+        View (class): Built in parent class for views
+    """
+    def get(self, request, product_id):
+        """Renders the product details page
+
+        Args:
+            request (object): HTTP request object
+
+        Returns:
+            method: renders products detail page
+        """
+
+        product = get_object_or_404(Product, pk=product_id)
+        
+        context = {
+            'product': product,
+        }
+
+        return render(request, 'products/product_details.html', context)
