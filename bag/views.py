09dd98c072c8398f77538cae0c157898ef7a8abd
4n4ru <ana.runje@live.com>
2022-01-22 14:45:08 +0000

    Feat: set up urls, basic view and template for bag.html

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,23 @@
+# Imports:
+# 3rd party:
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+# Internal:
+
+class Bag(View):
+    """A view to display the shopping bag page
+
+    Args:
+        View (class): Built in parent class for views
+    """
+    def get(self, request):
+        """Renders the shopping bag page
+
+        Args:
+            request (object): HTTP request object
+
+        Returns:
+            method: renders shopping bag page
+        """
+        return render(request, 'bag/bag.html')
