352bd9c7a33fe85797fc4c63ba36a710456da9bb
4n4ru <ana.runje@live.com>
2022-01-22 17:39:40 +0000

    Feat: Add form and view to add items to bag

--- bag/urls.py
+++ bag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Bag.as_view(), name='bag'),
+    path('add/<item_id>/', views.AddToBag.as_view(), name='add_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
 # Imports:
 # 3rd party:
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 
 # Internal:
@@ -21,3 +21,31 @@ class Bag(View):
             method: renders shopping bag page
         """
         return render(request, 'bag/bag.html')
+
+class AddToBag(View):
+    """Adding items to bag
+
+    Args:
+        View (class): Built in parent class for views
+    """
+    def post(self, request, item_id):
+        """Adding items to bag
+
+        Args:
+            request (object): HTTP request object
+            item_id (int): id of product being added to the bag
+
+        Returns:
+            method: redirects back to the redirect_url passed into the form
+        """
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        bag = request.session.get('bag', {})
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
+        request.session['bag'] = bag
+        return redirect(redirect_url)
